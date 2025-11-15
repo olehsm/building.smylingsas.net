bc5a2e6675da044038de98911705e7a90c6b04dc
olehsm <ohsmyl@gmail.com>
2025-11-15 23:10:31 +0000

    Add light dark toggle

--- building.smylingsas.net/assets/js/index.js
+++ building.smylingsas.net/assets/js/index.js
@@ -3,6 +3,7 @@ import { initModals, addModalOpenListener } from './modules/modals.js';
 import { initSearch } from './modules/search.js';
 import { initNetwork, updateNetworkInfo } from './modules/network.js';
 import { initBattery, updateBatteryInfo } from './modules/battery.js';
+import { initTheme, wireThemeButtons, wireThemeToggle } from './modules/theme.js';
 
 // Initialize modules when DOM is ready
 function onReady() {
@@ -11,6 +12,9 @@ function onReady() {
   initSearch();
   initNetwork();
   initBattery();
+  initTheme();
+  // Use a single tri-state theme toggle button when present
+  wireThemeToggle();
 
   // When modals open, allow manual refresh of their content
   addModalOpenListener((modalId) => {
